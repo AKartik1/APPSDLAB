425b437bea2c3b810a3faddbcc9163b79ee026eb
AKartik1 <120768680+AKartik1@users.noreply.github.com>
2025-01-28 01:44:22 +0530

    Update app.js

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,13 +1,23 @@
-import React from 'react';
-import Questions from './Questions';
-import './styles.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import HowToPlayPage from "./HowToPlayPage"; // Import your new page
+import Questions from "./Questions"; // Assuming you have this component
+import Navbar from "./Navbar"; // If you have a Navbar component
 
 function App() {
     return (
-        <div className="app-container">
-            <h1>Quiz Game</h1>
-            <Questions />
-        </div>
+        <Router>
+            <Navbar title="Quiz Game" /> {/* Show the navbar for navigation */}
+            <Switch>
+                <Route path="/how-to-play/Groups" component={HowToPlayPage} />
+                <Route path="/questions" component={Questions} />
+                {/* Add other routes as needed */}
+                <Route path="/" exact>
+                    <h1>Welcome to the Quiz Game!</h1>
+                    <p>Select a topic to get started.</p>
+                </Route>
+            </Switch>
+        </Router>
     );
 }
 
