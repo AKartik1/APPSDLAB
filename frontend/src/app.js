e411132492a49fab189fb039128f09dd90829aad
AKartik1 <120768680+AKartik1@users.noreply.github.com>
2025-01-28 01:39:11 +0530

    Add files via upload

---
+++ frontend/src/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Questions from './Questions';
+import './styles.css';
+
+function App() {
+    return (
+        <div className="app-container">
+            <h1>Quiz Game</h1>
+            <Questions />
+        </div>
+    );
+}
+
+export default App;
